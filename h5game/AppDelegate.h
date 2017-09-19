dec727be97502480ef24c76c132aadba76b35f46
shilei <shilei@shileidemacbook-pro.local>
2017-09-19 17:15:08 +0800

    Initial Commit

---
+++ h5game/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  h5game
+//
+//  Created by shilei on 2017/9/19.
+//  Copyright © 2017年 shilei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ h5game/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  h5game
+//
+//  Created by shilei on 2017/9/19.
+//  Copyright © 2017年 shilei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
